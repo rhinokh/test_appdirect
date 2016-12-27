ffef91b177757287d71de0115f2cc7f650e81afe
Virakraingsei Haisocheat <raingsei@oneflow.com>
2016-12-27 13:08:22 +0100

    Added request validator

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+
+from oauth2 import Consumer
+from oauth2 import Request as OauthRequest
+from oauth2 import Server as OauthServer
+from oauth2 import SignatureMethod_HMAC_SHA1
+
+request_oauth = ('oneflow-139996', 'LR7tqmp4bvKom3ZG')
+request_oauth_signature_method = SignatureMethod_HMAC_SHA1()
+oauth_server = OauthServer(
+    signature_methods={
+        request_oauth_signature_method.name: request_oauth_signature_method
+    }
+)
+
+
+def validate(oauth_request):
+    oauth_consumer = Consumer(key=request_oauth[0], secret=request_oauth[1])
+    try:
+        oauth_server.verify_request(
+            request=oauth_request,
+            consumer=oauth_consumer,
+            token='')
+        return True
+    except Exception:
+        pass
+
+    return False
+
+def validate_response(response):
+    if not response:
+        return False
+
+    oauth_request = OauthRequest.from_request(
+        http_method=response.request.method,
+        http_url=response.request.url,
+        headers=response.headers
+    )
+
+    return validate(oauth_request=oauth_request)
+
+def validate_request(request):
+    if not request:
+        return False
+
+    oauth_request = OauthRequest.from_request(
+        http_method=request.method,
+        http_url=request.base_url,
+        headers=request.headers,
+        parameters=dict(request.form.copy()),
+        query_string=request.query_string
+    )
+
+    import pdb
+    pdb.set_trace()
+
+    oauth_request['oauth_consumer_key'] = request_oauth[0]  # very important
+
+    return validate(oauth_request=oauth_request)
+
+
+app = Flask(__name__)
+
+@app.route('/create')
+def create():
+    response = validate_request(request=request)
+    return str(response)
+
+if __name__ == "__main__":
+    app.run()
